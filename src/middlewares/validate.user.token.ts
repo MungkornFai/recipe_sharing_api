b069bb8fb92f9903e379ffef0049b6b884433bb2
Mungkorn <phamungkorn0@gmail.com>
2024-12-25 08:37:28 +0700

    feat: implement token validate

--- src/middlewares/validate.user.token.ts
+++ src/middlewares/validate.user.token.ts
@@ -1,12 +1,15 @@
 import { NextFunction, Request, Response } from "express";
+import { decodeToken } from "../lib/session.token";
 
 export function authenticatToken(req: Request, res: Response, next: NextFunction) {
   const token = req.cookies.token;
   try {
     if (!token) {
-        res.status(400).json({ meassage: "unauthorize" });
+        res.status(400).json({ meassage: "unauthorize: user neeed to sig in first" });
         return;
     }
+    const user = decodeToken(token);
+    req.userId = user;
     next();
   } catch (error) {
     res.status(500).json({ message: "Error validating token", error });
