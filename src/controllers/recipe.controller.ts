5f0f4375ba01bb4e4af03c91b47b54fcd1cd18f2
Mungkorn <phamungkorn0@gmail.com>
2024-12-29 10:23:33 +0700

    feat: implement recipe comment enpoint

--- src/controllers/recipe.controller.ts
+++ src/controllers/recipe.controller.ts
@@ -1,10 +1,11 @@
 import type { Request, Response, NextFunction } from "express";
 
-import { Recipe } from "../types/recipe";
-import { createRecipe } from "../services/recipe.service";
-import { sendSuccess } from "../lib/response";
+import { Recipe, TQuery, TComment, TRecipeParams } from "../types/recipe";
+import { sendError, sendSuccess } from "../lib/response";
 import { db } from "../db";
-import { recipes } from "../db/schema/recipes";
+import { recipes, comments, commentRelations } from "../db/schema/recipes";
+import { eq, like, and } from "drizzle-orm";
+import { d } from "drizzle-kit/index-Z-1TKnbX";
 
 export async function handlerRecipeCreate(
   req: Request<{}, {}, Recipe>,
@@ -17,10 +18,91 @@ export async function handlerRecipeCreate(
       .insert(recipes)
       .values({
         ...body,
-        userId: req.userId?.userId,
+        userId: req.user?.userId,
       })
       .returning();
     sendSuccess(res, "Recipe created successfully", newRecipe, 201);
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function handlerRecipeGetAll(req: Request, res: Response, next: NextFunction) {
+  try {
+    const recipe = await db.select().from(recipes);
+    sendSuccess(res, "Recipes fetched successfully", recipe, 200);
+  } catch (error) {
+    next(error);
+  }
+}
+
+// get all recipes for specific user
+
+export async function handlerRecipeGetById(req: Request, res: Response, next: NextFunction) {
+  try {
+    if (!req.user || !req.user.userId) {
+      sendError(res, "Unauthorized: user needs to sign in first", 403);
+      return;
+    }
+    // Convert req.params.id to a number
+    const recipeId = parseInt(req.params.id, 10);
+    if (isNaN(recipeId)) {
+      sendError(res, "Invalid recipe ID", 400);
+      return;
+    }
+    const recipe = await db.select().from(recipes).where(eq(recipes.id, recipeId));
+    if (!recipe) {
+      sendError(res, "Recipe not found", 404);
+      return;
+    }
+
+    sendSuccess(res, "Recipe fetched successfully", recipe, 200);
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function handleSearchRecipe(
+  req: Request<{}, {}, {}, TQuery>,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const { title } = req.query;
+    let query = db.select().from(recipes);
+    if (title) {
+      query = query.where(like(recipes.title, `%${title}%`)) as any;
+    }
+    const recipesResult = await query;
+    sendSuccess(res, "Recipes fetched successfully", recipesResult, 200);
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function handlerRecipeComment(
+  req: Request<TRecipeParams, {}, TComment>,
+  res: Response,
+  next: NextFunction
+) {
+  const body = req.body;
+  const recipeId = req.params.id;
+  try {
+    if (isNaN(parseInt(recipeId))) {
+      sendError(res, "Invalid recipe ID", 400);
+      return;
+    }
+    const newComment = await db
+      .insert(comments)
+      .values({
+        comment: body.comment,
+        recipeId: parseInt(recipeId),
+        userId: req.user.userId,
+      })
+      .returning();
+    
+
+    sendSuccess(res, "Comment created successfully", newComment, 201);
   } catch (error) {
     next(error);
   }
